1109036662c4f89017e1fb1e207f98aa3271f20d
Frank Pigeon <frank.pigeonjr@gmail.com>
2022-02-05 20:57:20 -0600

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Aplayer from 'vue-aplayer'
+</script>
+
+<template>
+  <main>
+    <Aplayer :music="{
+    title: 'secret base~君がくれたもの~',
+    artist: 'Silent Siren',
+    src: 'https://moeplayer.b0.upaiyun.com/aplayer/secretbase.mp3',
+    pic: 'https://moeplayer.b0.upaiyun.com/aplayer/secretbase.jpg'
+  }"/>
+      
+  </main>
+</template>
