7b5bf2cdfe509240d55e3d03639e01c29d622df3
Frank Pigeon <frank.pigeonjr@gmail.com>
2022-02-05 23:07:42 -0600

    adds About page content

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About DJ Elder</h1>
+    <p>
+      Dj Elder is a veteran of the Christian hip hop scene. For this project, he
+      was looking for acapellas where he could remix these songs using his
+      original beats. The result is a boom bap hip hop album featuring various
+      artists. The beats were made with the legendary E-MU SP-1200, MPC60 and
+      the Isla S2400.
+    </p>
+    <h2>Contact</h2>
+    <p>
+      Email:
+      <a href="mailto:reactivate2@yahoo.com">reactivate2@yahoo.com</a>
+    </p>
   </div>
 </template>
 
