ef82ec63a4ec1e0dcc36768811ea6627714074f2
Frank Pigeon <frank.pigeonjr@gmail.com>
2022-02-06 19:03:20 -0600

    adds tailwind css and starts styles

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,27 +1,28 @@
 <template>
-  <div class="about">
-    <h1>About DJ Elder</h1>
-    <p>
-      Dj Elder is a veteran of the Christian hip hop scene. For this project, he
-      was looking for acapellas where he could remix these songs using his
-      original beats. The result is a boom bap hip hop album featuring various
-      artists. The beats were made with the legendary E-MU SP-1200, MPC60 and
-      the Isla S2400.
-    </p>
-    <h2>Contact</h2>
-    <p>
-      Email:
-      <a href="mailto:reactivate2@yahoo.com">reactivate2@yahoo.com</a>
-    </p>
+  <div class="px-3 md:px-0">
+    <h2 class="text-3xl font-bold mb-2">About DJ Elder</h2>
+    <div class="space-y-5 text-lg">
+      <p>
+        DJ Elder is a veteran of the Christian hip hop scene. For this project,
+        he was looking for acapellas where he could remix these songs using his
+        original beats.
+      </p>
+      <p>The result is a boom bap hip hop album featuring various artists.</p>
+      <p>
+        The beats were made with the legendary E-MU SP-1200, MPC60 and the Isla
+        S2400.
+      </p>
+      <h2 class="text-2xl font-bold">Contact</h2>
+      <p>
+        <span class="font-bold">email: </span>
+        <a
+          class="text-blue-500 hover:text-blue-800"
+          href="mailto:reactivate2@yahoo.com"
+          >reactivate2@yahoo.com</a
+        >
+      </p>
+    </div>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+<style></style>
